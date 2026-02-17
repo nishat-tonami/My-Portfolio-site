4a9326a213c56f99d45cf26073940fcb917c1f3f
nishat-tonami <nishat.tonami121@gmail.com>
2026-02-18 02:14:12 +0600

    added smooth scroll functionality

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,6 +1,8 @@
 const themeBtn=document.getElementById("theme-btn");
 const body=document.body;
 
+const navLinks=document.querySelectorAll('.navlinks a');
+
 themeBtn.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
 
@@ -9,4 +11,18 @@ themeBtn.addEventListener("click", () => {
   } else {
     themeBtn.textContent="🌙";
   }
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click',function(e) {
+    e.preventDefault();
+    
+    const targetId=this.getAttribute('href').substring(1);
+    const targetSection=document.getElementById(targetId);
+
+    window.scrollTo({
+      top:targetSection.offsetTop-45,
+      behavior:'smooth'
+    });
+  });
 });
